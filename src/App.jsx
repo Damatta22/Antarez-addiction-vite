1349a3458b570f88e03c705109fed188b73c1985
Damatta22 <flaviodamataflv@gmail.com>
2023-09-09 10:31:27 -0300

    antarez

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {} from 'react'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Here is a project that is under development</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
